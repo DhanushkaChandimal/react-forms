3d230d63b75c150361eba46e91b5b3d5c78ad3cb
Dhanushka <dhanushkamcr@gmail.com>
2025-08-04 04:15:36 -0500

    Implement find and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import CreatePost from './components/CreatePost'
+import FindDeletePost from './components/FindDeletePost'
 
 function App() {
 
   return (
     <>
       <CreatePost/>
+      <FindDeletePost/>
     </>
   )
 }
